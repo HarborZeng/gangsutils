ba8d7d7340bd8d7bd28ba3392da6de4f690b55c9
harborzeng <harborzeng@qq.com>
2021-08-05 23:32:39 +0800

    实验田

--- gangsutils-useless/src/main/scala/cn/tellyouwhat/gangsutils/useless/MyApp.scala
+++ gangsutils-useless/src/main/scala/cn/tellyouwhat/gangsutils/useless/MyApp.scala
@@ -1,9 +1,12 @@
 package cn.tellyouwhat.gangsutils.useless
 
+import cn.tellyouwhat.gangsutils.common.gangfunctions.retry
 import cn.tellyouwhat.gangsutils.common.helper.{Timeit, TimeitLogger}
 import cn.tellyouwhat.gangsutils.common.logger.{BaseLogger, GangLogger, LogLevel}
 import cn.tellyouwhat.gangsutils.common.logger.SupportedLogDest.PRINTLN_LOGGER
 import org.apache.hadoop.fs.Path
+
+import java.sql.SQLDataException
 
 /**
  * 代码实验田
@@ -20,9 +23,15 @@ class MyApp extends Timeit {
     logger.success("success")
     logger.warning("warning")
     logger.error("error")
-//    logger.critical("critical")
+    //    logger.critical("critical")
 
     logger.info(new Path("path/to", "_SUCCESS").toString)
+
+    retry(2)(fun())
+  }
+
+  def fun(): Nothing = {
+    throw new SQLDataException("haha")
   }
 }
 
