3af7809552c9d91132f18f4d528ac42c33149aed
harborzeng <harborzeng@qq.com>
2021-08-07 00:01:57 +0800

    add MappableTest

--- gangsutils-common/src/test/scala/cn/tellyouwhat/gangsutils/common/cc/MappableTest.scala
+++ gangsutils-common/src/test/scala/cn/tellyouwhat/gangsutils/common/cc/MappableTest.scala
@@ -1,5 +1,16 @@
 package cn.tellyouwhat.gangsutils.common.cc
 
-/*class MappableTest extends FunSuite {
+import cn.tellyouwhat.gangsutils.common.gangfunctions.ccToMap
+import cn.tellyouwhat.gangsutils.common.helper.chaining.{PipeIt, TapIt}
+import org.scalatest.funsuite.AnyFunSuite
 
-}*/
+
+class MappableTest extends AnyFunSuite  {
+
+  case class Person(name: String, age: Int) extends Mappable
+
+  test("Person(\"harbor\", 10) should can be converted to map") {
+    assert(Person("harbor", 10) |> ccToMap |> (m => m("name") == "harbor" && m("age") == 10))
+  }
+
+}
