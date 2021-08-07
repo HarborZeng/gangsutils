d88595466af7dab85231ea808449c3949ce79e30
harborzeng <harborzeng@qq.com>
2021-08-07 17:49:52 +0800

    添加一些测试

--- gangsutils-common/src/test/scala/cn/tellyouwhat/gangsutils/common/cc/MappableTest.scala
+++ gangsutils-common/src/test/scala/cn/tellyouwhat/gangsutils/common/cc/MappableTest.scala
@@ -2,15 +2,21 @@ package cn.tellyouwhat.gangsutils.common.cc
 
 import cn.tellyouwhat.gangsutils.common.gangfunctions.ccToMap
 import cn.tellyouwhat.gangsutils.common.helper.chaining.PipeIt
-import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
 
-class MappableTest extends AnyFunSuite {
+case class Person(name: String, age: Int) extends Mappable
 
-  case class Person(name: String, age: Int) extends Mappable
+class MappableTest extends AnyFlatSpec with Matchers {
 
-  test("Person(\"harbor\", 10) should can be converted to an equivalent map") {
-    assert(Person("harbor", 10) |> ccToMap |> (m => m("name") == "harbor" && m("age") == 10))
+
+  "Person(\"harbor\", 10)" should "can be converted to an equivalent map" in {
+    val harborCC = Person("harbor", 10)
+    val harborMap = harborCC |> ccToMap
+    harborMap should have size 2
+    harborMap("name") shouldEqual harborCC.name
+    harborMap("age") shouldEqual harborCC.age
   }
 
 }
