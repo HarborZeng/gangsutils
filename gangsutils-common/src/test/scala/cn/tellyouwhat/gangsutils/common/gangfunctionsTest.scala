cee91916e025b2329d09a5818e5b2ab6c34748d8
HarborZeng <HarborZeng@users.noreply.github.com>
2021-08-09 10:44:34 +0800

    增加测试用例描述

--- gangsutils-common/src/test/scala/cn/tellyouwhat/gangsutils/common/gangfunctionsTest.scala
+++ gangsutils-common/src/test/scala/cn/tellyouwhat/gangsutils/common/gangfunctionsTest.scala
@@ -207,7 +207,7 @@ class gangfunctionsTest extends AnyFlatSpec with Matchers {
     ext_count should contain allOf("txt" -> 2, "pdf" -> 1, "docx" -> 2, "ppt" -> 3)
   }
 
-  it should "printOrLog" in {
+  "printOrLog" should "print to stdout ablout the built log content or use logger(BaseLogger) to do a log action if the parameter logger in filled" in {
     val stream = new ByteArrayOutputStream()
     Console.withOut(stream) {
       gangfunctions.printOrLog("content", LogLevel.TRACE)
@@ -222,7 +222,7 @@ class gangfunctionsTest extends AnyFlatSpec with Matchers {
   }
 
 
-  it should "retry" in {
+  "retry" should "inkove a function with tolerance of n times failure and printOrLog if it did" in {
     gangfunctions.retry(3)(1 + 1) match {
       case Failure(_) =>
       case Success(v) => v shouldBe 2
