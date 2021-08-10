6b703e058e317c0a4667bb52689aa090d1952b26
harborzeng <harborzeng@qq.com>
2021-08-10 10:58:17 +0800

    add tests

--- gangsutils-common/src/test/scala/cn/tellyouwhat/gangsutils/common/gangfunctionsTest.scala
+++ gangsutils-common/src/test/scala/cn/tellyouwhat/gangsutils/common/gangfunctionsTest.scala
@@ -2,7 +2,9 @@ package cn.tellyouwhat.gangsutils.common
 
 import cn.tellyouwhat.gangsutils.common.cc.Mappable
 import cn.tellyouwhat.gangsutils.common.exceptions.GangException
+import cn.tellyouwhat.gangsutils.common.helper.chaining.TapIt
 import cn.tellyouwhat.gangsutils.common.logger.{GangLogger, LogLevel}
+import com.mysql.cj.jdbc.exceptions.CommunicationsException
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql.SparkSession
 import org.scalatest.PrivateMethodTester
@@ -10,6 +12,8 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import java.io.ByteArrayOutputStream
+import java.time.{LocalDateTime, ZoneId}
+import java.util.Properties
 import scala.util.{Failure, Success}
 
 class gangfunctionsTest extends AnyFlatSpec with Matchers with PrivateMethodTester {
@@ -251,6 +255,15 @@ class gangfunctionsTest extends AnyFlatSpec with Matchers with PrivateMethodTest
 
   implicit val spark: SparkSession = SparkSession.builder().master("local").getOrCreate()
 
+  val workingDirectory: Path = {
+    val getFS = PrivateMethod[FileSystem]('getFS)
+    val fs = gangfunctions invokePrivate getFS(spark)
+    fs.getWorkingDirectory
+  }
+
+  val sparkJobDirPath = new Path(workingDirectory, "src/test/resources/spark_job_dir")
+  val doesNotExistsDirPath = new Path(workingDirectory, "src/test/resources/does_not_exists")
+
   "getFS" should "get you a hadoop file system object" in {
     val getFS = PrivateMethod[FileSystem]('getFS)
     val fs = gangfunctions invokePrivate getFS(spark)
@@ -258,33 +271,85 @@ class gangfunctionsTest extends AnyFlatSpec with Matchers with PrivateMethodTest
   }
 
   "isPathExists" should "test a string path existence" in {
-    gangfunctions.isPathExists("file:/") shouldBe true
+    val sparkJobDirStringPath = sparkJobDirPath.toString
+    val doesNotExistsDirStringPath = doesNotExistsDirPath.toString
+    gangfunctions.isPathExists(sparkJobDirStringPath) shouldBe true
+    gangfunctions.isPathExists(doesNotExistsDirStringPath) shouldBe false
   }
 
   it should "test a Path existence" in {
-    gangfunctions.isPathExists(new Path("file:/")) shouldBe true
+    gangfunctions.isPathExists(sparkJobDirPath) shouldBe true
+    gangfunctions.isPathExists(doesNotExistsDirPath) shouldBe false
   }
 
   "fileModifiedTime" should "get mtime of string path" in {
-    gangfunctions.fileModifiedTime("file:/") match {
+    gangfunctions.fileModifiedTime(sparkJobDirPath.toString) match {
       case Left(_) =>
       case Right(t) => t shouldBe >= (0L)
     }
-    gangfunctions.fileModifiedTime("file:/a") match {
+    gangfunctions.fileModifiedTime(doesNotExistsDirPath.toString) match {
       case Left(e) => a [GangException] should be thrownBy (throw e)
       case Right(_) =>
     }
   }
 
   it should "get mtime of Path" in {
-    gangfunctions.fileModifiedTime(new Path("file:/")) match {
+    gangfunctions.fileModifiedTime(sparkJobDirPath) match {
       case Left(_) =>
       case Right(t) => t shouldBe >= (0L)
     }
-    gangfunctions.fileModifiedTime(new Path("file:/a")) match {
+    gangfunctions.fileModifiedTime(doesNotExistsDirPath) match {
       case Left(e) => a [GangException] should be thrownBy (throw e)
       case Right(_) =>
     }
+  }
+
+  "isSparkSaveDirExists" should "test a spark save dir string path existence" in {
+    gangfunctions.isSparkSaveDirExists(sparkJobDirPath.toString) shouldBe true
+    gangfunctions.isSparkSaveDirExists(doesNotExistsDirPath.toString) shouldBe false
+  }
+
+  "isSparkSaveDirModifiedToday" should "test a spark save dir whether modified today" in {
+    val getFS = PrivateMethod[FileSystem]('getFS)
+    val fs = gangfunctions invokePrivate getFS(spark)
+    val todayTimeMill = LocalDateTime.now()
+      .atZone(ZoneId.systemDefault())
+      .toInstant.toEpochMilli
+    fs.setTimes(new Path(sparkJobDirPath, "_SUCCESS"), todayTimeMill, todayTimeMill)
+    gangfunctions.isSparkSaveDirModifiedToday(sparkJobDirPath.toString) shouldBe true
+
+    val yesterdayTimeMill = LocalDateTime.now()
+      .atZone(ZoneId.systemDefault())
+      .minusDays(1).minusHours(1)
+      .toInstant.toEpochMilli
+    fs.setTimes(new Path(sparkJobDirPath, "_SUCCESS"), yesterdayTimeMill, yesterdayTimeMill)
+    gangfunctions.isSparkSaveDirModifiedToday(sparkJobDirPath.toString) shouldBe false
+
+    a [GangException] should be thrownBy gangfunctions.isSparkSaveDirModifiedToday(doesNotExistsDirPath.toString)
+  }
+
+  "isSparkSaveDirModifiedWithinNHours" should "test a spark save dir whether modified within n hours" in {
+    val getFS = PrivateMethod[FileSystem]('getFS)
+    val fs = gangfunctions invokePrivate getFS(spark)
+    val twoHoursAgoTimeMill = LocalDateTime.now()
+      .atZone(ZoneId.systemDefault())
+      .minusHours(2)
+      .toInstant.toEpochMilli
+    fs.setTimes(new Path(sparkJobDirPath, "_SUCCESS"), twoHoursAgoTimeMill, twoHoursAgoTimeMill)
+    gangfunctions.isSparkSaveDirModifiedWithinNHours(sparkJobDirPath.toString)(3) shouldBe true
+    gangfunctions.isSparkSaveDirModifiedWithinNHours(sparkJobDirPath.toString)(1) shouldBe false
+
+    a [GangException] should be thrownBy gangfunctions.isSparkSaveDirModifiedWithinNHours(doesNotExistsDirPath.toString)(1)
+  }
+
+  "getMysql5Conn" should "connect to mysql server" in {
+    val p = new Properties() |! (_.setProperty("username", "the_name")) |! (_.setProperty("password", "the_pass"))
+    a [CommunicationsException] should be thrownBy gangfunctions.getMysql5Conn(p, "localhost", "db_name")
+  }
+
+  "getMysql8Conn" should "connect to mysql server" in {
+    val p = new Properties() |! (_.setProperty("username", "the_name")) |! (_.setProperty("password", "the_pass"))
+    a [CommunicationsException] should be thrownBy gangfunctions.getMysql8Conn(p, "localhost", "db_name")
   }
 
 }
